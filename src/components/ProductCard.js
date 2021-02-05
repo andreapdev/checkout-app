f3b3e504448774d1569b0f38762654a544fb720b
andreapdev <andreap.dev@gmail.com>
2021-02-05 12:51:02 +0100

    ProductCard started

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const ProductCard = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
-    return (
-        <>
-            <div>This is the ProductCard</div>
-        </>
-    );
-}
+const imgSrc =
+  "https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png";
+
+const imgStyle = {
+  maxHeight: '200px'
+};
+const ProductCard = () => {
+  return (
+    <div>
+      <img src={imgSrc} alt="product" style={imgStyle} />
+      <p>Product name</p>
+      <div>
+        <input type="number" min="1" defaultValue="1"></input>
+        <button>
+          <FontAwesomeIcon icon={faShoppingBasket} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ProductCard;
