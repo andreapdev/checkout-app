bb07bbd4df4d5c23b7d53f55c6e44f24e5c5e5c8
andreapdev <andreap.dev@gmail.com>
2021-02-05 12:04:21 +0100

    App.js routes implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
+import Home from "./screens/Home";
+import Checkout from "./screens/Checkout";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
