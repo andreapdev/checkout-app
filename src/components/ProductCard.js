98457f95c270c084ab3b17b31415245cc7bd81bb
andreapdev <andreap.dev@gmail.com>
2021-02-06 16:49:36 +0100

    ProductDeck component setup

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,22 +1,40 @@
-import React from 'react';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
 const imgSrc =
   "https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png";
 
 const imgStyle = {
-  maxHeight: '200px'
+  maxHeight: "200px"
 };
-const ProductCard = () => {
+const ProductCard = ({ id, name, price, handleProductAdd }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleChange = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  // const handleClick = (e) => {
+  //   e.preventDefault();
+  //   handleProductAdd(id, name, quantity, price);
+  // };
+
   return (
-    <div>
+    <div id={id}>
       <img src={imgSrc} alt="product" style={imgStyle} />
-      <p>Product name</p>
+      <p>{name}</p>
+      <p>{price}</p>
       <div>
-        <input type="number" min="1" defaultValue="1"></input>
-        <button>
+        <input
+          type="number"
+          min="1"
+          value={quantity}
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <button 
+        // onClick={(e) => handleClick(e)}
+        >
           <FontAwesomeIcon icon={faShoppingBasket} />
         </button>
       </div>
